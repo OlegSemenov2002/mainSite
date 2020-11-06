d0cdde39581c74d023cec26855f82a246a0e24a5
Oleg Semenov <rsniffer@mail.ru>
2020-11-06 18:53:59 +0300

    Story 1: добавили редактор сетки

--- func.js
+++ func.js
@@ -71,10 +71,16 @@ x = Math.max.apply(null, x);
 	console.log(grid);
 	return grid
 }
-function generate(grid){
+function generate(place,grid){
+	let place1 = document.getElementById(place);
+	let text = document.createTextNode(grid);
+	let div = document.createElement('div');
+	div.onclick = select;
+	
+
 	let y = grid.length,
 	x = grid[0].length,
-	table = document.body.querySelector('table'),
+	table = document.createElement('table'),
 	tr = [];
 
 	for(let i = 0; i<y; i++){
@@ -89,16 +95,78 @@ function generate(grid){
 			tr[i].appendChild(td1);
 		}
 		table.appendChild(tr[i]);
-	}	
+	}
+	table.setAttribute('class','menu');
+	div.appendChild(table);
+	place1.appendChild(div);
+
 }
 
-let grid = new generateGrid(4,4);
-console.log(grid);
-let grid1 = new enlargeCell(grid,'0x0',4,1);
+let grid00 = new generateGrid(3,3);
+let grid01 = new enlargeCell(grid00,'0x0',2,2);
+
+let grid10 = new generateGrid(4,4);
+let grid11 = new enlargeCell(grid10,'0x0',3,3);
+
+let grid20 = new generateGrid(4,4);
+let grid21 = new enlargeCell(grid20,'0x0',2,2);
+
+let grid30 = new generateGrid(4,4);
+let grid31 = new enlargeCell(grid30,'1x1',2,2);
+
+function select(){
+	let el = document.getElementById('down'),
+	cell = document.getElementById('selected'),
+	div = document.createElement('div'),
+	clone = this.cloneNode(true);
+	let td = clone.querySelectorAll('td');
+	for(let el of td){
+		el.onclick = function(){
+			el.style.border= '1px solid black';
+
+			if(document.querySelectorAll('.on').length>=1){
+				let on = document.querySelectorAll('.on');
+				for(let el of on){
+					el.style.border= '0px solid black';
+					el.removeAttribute('class');
+				}
+			}
+			this.setAttribute('class', 'on');
+			let buttons = document.querySelectorAll('button');
+			for(let button of buttons){
+				button.removeAttribute('disabled');
+			}
+
+
+		}
+	}
+
+    
+	cell.remove();
+	div.id = 'selected';
+	clone.id ='selected';
+	div.appendChild(clone);
+	el.appendChild(div);
+}
+function addImage(object){
+	let image = document.getElementById('image');
+	image.style['background-image'] = 'url('+object+'.jpg)';
+
+}
+function confrm(){
+	let cell = document.querySelector('.on');
+	cell.style['background-image'] = image.style['background-image'];
+
+}
+
+
 /*console.log(grid1);
 let arr =gridToArray(grid1);
 console.log(arr);
 arrayToGrid(arr);*/
-generate(grid1);
+generate('heh',grid01);
+generate('heh',grid11);
+generate('heh',grid21);
+generate('heh',grid31);
 
-
+alert();
